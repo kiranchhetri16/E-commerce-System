e1cfe331588ef71de2d599aadf67f3d7221c75d7
kiran chhetri <kiran.bntech@gmail.com>
2026-03-12 13:37:59 +0545

    created and updated the login form compoment

--- src/pages/user/auth/Login.tsx
+++ src/pages/user/auth/Login.tsx
@@ -1,4 +1,6 @@
 import { useState, type ChangeEvent } from "react";
+import { InputType } from "../../../components/form-component/InputTypeProps";
+import CustomButton from "../../../components/CustomButton";
 
 export const Login = () => {
   const [formData, setFormData] = useState({
@@ -6,63 +8,48 @@ export const Login = () => {
     email: "",
     password: "",
   });
-    // const hanldeChange = (e: ChangeEvent<HTMLInputElement >) => {
-    //   const { name, value } = e.target;
-    //   setFormData((prev) => ({ ...prev, [name]: value }));
-    // };
+    const hanldeChange = (e: ChangeEvent<HTMLInputElement >) => {
+      const { name, value } = e.target;
+      setFormData((prev) => ({ ...prev, [name]: value }));
+    };
 
     const handleSubmit = (e: React.FormEvent) => {
       e.preventDefault();
       console.log("Login success", formData);
     };
   return (
-    <div className="h-150 w-100 border border-bg">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
-        <div>
-          <label>Username</label>
-          <input
-            type="text"
-            name="username"
-            value={formData.username}
-            onChange={(e) =>
-              setFormData({
-                ...formData,
-                username: e.target.value,
-              })
-            }
-            className="border border-red-300 p-2 rounded-sm"
-          />
-        </div>
-        <div className="">
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={(e) =>
-              setFormData({
-                ...formData,
-                email: e.target.value,
-              })
-            }
-            placeholder="Enter your email"
-            className="border border-red-300 p-2 rounded-sm"
-          />
-        </div>
-        <div className="">
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={(e) =>
-              setFormData({ ...formData, password: e.target.value })
-            }
-            placeholder="Enter password"
-            className="border border-red-300 p-2 rounded-sm"
-          />
-        </div>
-        <button type="submit">Login</button>
+    <div className="h-screen w-full bg-primary flex justify-center  items-center">
+      <form onSubmit={handleSubmit} className="flex w-100 flex-col gap-4 border border-border shadow-custom rounded-md p-4 bg-white">
+     
+        <InputType
+        label="User Name"
+        type="text"
+        name="username"
+        value={formData.username}
+        placeholder="Enter user Name"
+        handleInputChange={hanldeChange }
+        />
+
+
+      
+        <InputType 
+        label="Email"
+        name="email"
+        value={formData.email}
+        placeholder="Enter you email"
+        handleInputChange={hanldeChange}
+
+        />
+        
+        <InputType
+        label="Password"
+        name="password"
+        value={formData.password}
+        placeholder="ENter password"
+        handleInputChange={hanldeChange}
+        
+        />
+        <CustomButton className="w-full flex justify-center items-center" type="submit">Submit</CustomButton>
       </form>
     </div>
   );
